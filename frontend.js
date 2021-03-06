bdc1433f4d409afc87649ddf56e508a04e479dce
Julius W <julius.wunderlich@web.de>
2021-03-06 23:53:00 +0100

    first backend function

---
+++ frontend.js
@@ -0,0 +1,17 @@
+var text = ["public", "static", ...];
+
+
+
+
+// POST: daraus den aufruf bauen, den wir brauchen sobald ein Code abgeschlossen ist 
+const userAction = async () => {
+    const response = await fetch('http://example.com/movies.json', {
+      method: 'POST',
+      body: myBody, // string or object
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    const myJson = await response.json(); //extract JSON from the http response
+    // do something with myJson
+  }
---
+++ src/app.js
@@ -0,0 +1,25 @@
+let express = require('express');
+let app = express();
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+// where we land initially
+app.get('/',(req,res)=>{
+    res.status(200).send('Hello World!\n');
+});
+
+app.get("/codeSnippet", (req, res, next) => {
+    res.json(
+        {
+            "id":"00001",
+            "lang":"java",
+            "code": ["class", " ", "MyClass", " ", "{","}"]
+        }
+        )
+})
+
+["class", " ", "MyClass", " ", "{","}"]
