baec7199c263dd18b53ef23ac70140e6ff921398
Julius W <julius.wunderlich@web.de>
2021-03-07 01:45:36 +0100

    progress on gist request

--- src/app.js
+++ src/app.js
@@ -1,16 +1,18 @@
 const express = require('express');
-const MongoClient = require('mongodb').MongoClient;
+const axios = require('axios');
 
 const app = express();
 const port = 3000;
 
 app.listen(port, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at port 3000`);
 });
 
 // where we land initially
 app.get('/',(req,res) => {
     res.status(200).send('Hello World!\n');
+    //pubGists();
+
 });
 
 app.get("/codeSnippet", (req, res, next) => {
@@ -22,3 +24,33 @@ app.get("/codeSnippet", (req, res, next) => {
         }
         )
 });
+
+
+// load all public gists
+//pubGists = axios.get('https://jsonplaceholder.typicode.com/todos/1')
+pubGists = axios.get('https://api.github.com/gists/public')
+  .then(response => {
+    console.log("we got a response");
+    // public gist registry just contains url to user gists (no code)
+    //for(var gist of response.data){
+        axios.get(response.data[0].url).then(response => {
+            // check if useful file types in user gist
+            for (file of response.data.files) {
+                console.log(file.type);
+                // todo check what filetypes there are
+            }
+        })
+        .catch(error => {
+            console.log(error);
+        })
+    //}
+
+  })
+  .catch(error => {
+    console.log(error);
+  });
+
+// splits a huge code snippet on whitespaces and line breaks
+function splitCode(code) {
+
+}
