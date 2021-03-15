c6eb0f30a478e1c9a85f57285eeaa09c4b7d33e1
Niklas Lühr <n.luehr@campus.lmu.de>
2021-03-15 14:12:18 +0100

    Wagenrücklauf implementiert

--- frontend.js
+++ frontend.js
@@ -8,10 +8,8 @@ const indent_px = 25
 let paras = {}
 
 let line_idx = 0
-let token_idx = 0
 let current_line = text[line_idx][0]
 let cursor_pos = 0
-let whitespace_next = false
 
 window.onload = function (event) {
     for (l = 0; l < text.length; l++) {
@@ -28,17 +26,26 @@ window.onload = function (event) {
     }
 };
 
-document.addEventListener('keyup', function (event) {
+document.addEventListener('keydown', function (event) {
     let key = event.key
-    document.getElementById("key_label").innerHTML = key
+
+    if (key === "Enter") {
+        if (cursor_pos >= current_line.length) {
+            cursor_pos = 0
+            line_idx++
+            current_line = text[line_idx][0]
+            document.getElementById("textField").value = ""
+        }
+    }
 
     if (key === current_line.charAt(cursor_pos)) {
         forwardCursor()
-        document.getElementById("forward").innerHTML = "forward: " + cursor_pos
-        document.getElementById("current_token").innerHTML = current_token
         if (key === " ") {
             document.getElementById("textField").value = ""
         }
+    } else {
+        // TODO: Tippfehler
+
     }
 
 });
@@ -46,12 +53,10 @@ document.addEventListener('keyup', function (event) {
 function forwardCursor() {
     cursor_pos++
     paras[line_idx].innerHTML = '<span style=\"background-color:#00ff00;\">' + text[line_idx][0].slice(0, cursor_pos) + '</span>' + text[line_idx][0].slice(cursor_pos, current_line.length)
-
 }
 
 // POST: daraus den aufruf bauen, den wir brauchen sobald ein Code abgeschlossen ist 
 const get_new_snippet = async () => {
-    document.getElementById("code").innerHTML = "loading new snippet..."
     const response = await fetch('localhost:3000/codeSnippet', {
         method: 'POST',
         body: myBody, // string or object
