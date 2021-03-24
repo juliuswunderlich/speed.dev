48ffadf54c0f10709f5c1c6df4b5c0d299e69d81
Unknown <julius.wunderlich@web.de>
2021-03-24 23:57:50 +0100

    :fire: move exactly one freaggin foile

--- frontend/frontend.js
+++ frontend/frontend.js
@@ -4,26 +4,18 @@ const text = [
     ["System.out.println(\"Hello World!\");", 1],
     ["}", 0]
 ]
-const indent_px = 25
-let paras = {}
+const INDENT_PX = 25
+const HIGHLIGHT_COLOR = "#e79f0b"
+const CORRECT_COLOR = "#00ff00"
+const INCORRECT_COLOR = "#ff0000"
 
+let paras = {}
 let line_idx = 0
 let current_line = text[line_idx][0]
 let cursor_pos = 0
 
 window.onload = function (event) {
-    for (l = 0; l < text.length; l++) {
-        line = text[l]
-        let para = document.createElement("p")
-        paras[l] = para
-        if (line[1] > 0) {
-            indent = line[1] * indent_px
-            para.style.marginLeft = indent + "px"
-        }
-        let text_node = document.createTextNode(line[0])
-        para.appendChild(text_node);
-        document.getElementById("code_field").appendChild(para)
-    }
+    displaySnippet()
 };
 
 document.addEventListener('keydown', function (event) {
@@ -34,40 +26,95 @@ document.addEventListener('keydown', function (event) {
             cursor_pos = 0
             line_idx++
             current_line = text[line_idx][0]
+            highlightFirstCharacter()
             document.getElementById("textField").value = ""
         }
     }
 
     if (key === current_line.charAt(cursor_pos)) {
-        forwardCursor()
+        forwardCursor(true)
         if (key === " ") {
             document.getElementById("textField").value = ""
         }
     } else {
-        // TODO: Tippfehler
+        if (key === "Backspace") {
+            reverseCursor()
+        } else {
+            forwardCursor(false)
+        }
+
 
     }
 
 });
 
-function forwardCursor() {
+function displaySnippet() {
+    document.getElementById("code_field").innerHTML = ''
+    for (l = 0; l < text.length; l++) {
+        line = text[l]
+        let para = document.createElement("p")
+        paras[l] = para
+        if (line[1] > 0) {
+            indent = line[1] * INDENT_PX
+            para.style.marginLeft = indent + "px"
+        }
+        let text_node = document.createTextNode(line[0])
+        para.appendChild(text_node);
+        document.getElementById("code_field").appendChild(para)
+    }
+    highlightFirstCharacter()
+}
+
+function forwardCursor(correct) {
     cursor_pos++
-    paras[line_idx].innerHTML = '<span style=\"background-color:#00ff00;\">' + text[line_idx][0].slice(0, cursor_pos) + '</span>' + text[line_idx][0].slice(cursor_pos, current_line.length)
+    if (correct) {
+        color = CORRECT_COLOR
+    } else {
+        color = INCORRECT_COLOR
+    }
+    paras[line_idx].innerHTML = '<span style=\"color:' + color + ';\">'
+        + text[line_idx][0].slice(0, cursor_pos)
+        + '</span>'
+        + '<span style=\"background-color:' + HIGHLIGHT_COLOR + ';\">'
+        + text[line_idx][0].slice(cursor_pos, cursor_pos + 1)
+        + '</span>'
+        + text[line_idx][0].slice(cursor_pos + 1, current_line.length)
+}
+
+function reverseCursor() {
+
+
+}
+
+function highlightFirstCharacter() {
+    paras[line_idx].innerHTML = '<span style=\"background-color:' + HIGHLIGHT_COLOR + ';\">'
+        + text[line_idx][0].slice(0, cursor_pos + 1)
+        + '</span>'
+        + text[line_idx][0].slice(cursor_pos + 1, current_line.length)
+}
+
+//for testing
+function get_new_snippet() {
+    paras = {}
+    line_idx = 0
+    current_line = text[line_idx][0]
+    cursor_pos = 0
+    displaySnippet()
 }
 
 // POST: daraus den aufruf bauen, den wir brauchen sobald ein Code abgeschlossen ist 
-const get_new_snippet = async () => {
-    const response = await fetch('localhost:3000/codeSnippet', {
-        method: 'POST',
-        body: myBody, // string or object
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    });
-    const myJson = await response.json(); //extract JSON from the http response
-    const snippet = JSON.parse(myJson)
-    text = snippet.code_array
-    document.getElementById("code").innerHTML = snippet.code
-
-
-}
+// const get_new_snippet = async () => {
+//     const response = await fetch('localhost:3000/codeSnippet', {
+//         method: 'POST',
+//         body: myBody, // string or object
+//         headers: {
+//             'Content-Type': 'application/json'
+//         }
+//     });
+//     const myJson = await response.json(); //extract JSON from the http response
+//     const snippet = JSON.parse(myJson)
+//     text = snippet.code_array
+//     document.getElementById("code").innerHTML = snippet.code
+//
+//     displaySnippet()
+// }
