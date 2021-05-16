4b0f5ff442364124fdd2d95d9f4cfa469b5cd6f4
Julius W <julius.wunderlich@web.de>
2021-05-16 23:52:31 +0200

    fixed /code route :massage:

--- README.md
+++ README.md
@@ -4,8 +4,9 @@ Learn how to code hella fast (fingerwise not like brainwise)!
 
 ## Docker instructions
 ```
- docker build -t speeddev .
- docker run -p 3000:3000 -it speeddev
+docker build -t speeddev .
+docker run -p 3000:3000 -it speeddev
+
 ```
 Alternatively, use -v to mount the directory on host as a volume inside the image,
 so we don't need to rebuild and rerun the docker image after changes to the source code:
--- backend/backend.go
+++ backend/backend.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"log"
-	"net/http"
 	"utils"
 
 	"github.com/gin-gonic/contrib/static"
@@ -16,7 +15,7 @@ type Config struct {
 }
 
 func (config *Config) SetDefault() {
-	config.Port = "3000"
+	config.Port = ":3000"
 	config.StaticFolder = "../frontend"
 	config.IndexFile = "../index.html"
 }
@@ -27,7 +26,6 @@ func main() {
 	// use default config for now
 	config := Config{}
 	config.SetDefault()
-	router.Run(config.Port)
 
 	// frontend
 	router.Use(static.Serve("/", static.LocalFile(config.StaticFolder, true)))
@@ -35,53 +33,14 @@ func main() {
 
 	// define some routes
 	router.GET("/code", codesRequestHandler)
-
-	/*
-		router.POST("/somePost", posting)
-		router.PUT("/somePut", putting)
-		router.DELETE("/someDelete", deleting)
-		router.PATCH("/somePatch", patching)
-		router.HEAD("/someHead", head)
-		router.OPTIONS("/someOptions", options)
-
-		// By default it serves on :8080 unless a
-		// PORT environment variable was defined.
-		router.Run()
-		// router.Run(":3000") for a hard coded port
-
-		/*
-		fileServer := http.FileServer(http.Dir("../frontend/static"))
-		http.Handle("/", fileServer)
-		http.HandleFunc("/codes", codesRequestHandler)
-
-		fmt.Printf("Starting server at port 3000\n")
-		if err := http.ListenAndServe(":3000", nil); err != nil {
-			log.Fatal(err)
-		}
-	*/
+	router.Run(config.Port)
 }
 
-func defaultHandler(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"message": "pong",
-	})
-}
-
-func codesRequestHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/codes" {
-		http.Error(w, "404 not found.", http.StatusNotFound)
-		return
-	}
-
-	if r.Method != "GET" {
-		http.Error(w, "Method is not supported.", http.StatusNotFound)
-		return
-	}
-
+func codesRequestHandler(c *gin.Context) {
 	dir := "utils/Codes/files"
 	err, js := utils.ReadAllDirFilesIntoJson(dir)
 	if err != nil {
 		log.Fatal(err)
 	}
-	w.Write(js)
+	c.Writer.Write(js)
 }
