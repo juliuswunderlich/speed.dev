6a9cfc0bc87c1193f3ca1301bef9536ee549e7f9
Niklas Lühr <n.luehr@campus.lmu.de>
2021-05-17 00:55:22 +0200

    makes future lines transparent, adds return symbol at eol

--- frontend/static/components/CodeDisplay.js
+++ frontend/static/components/CodeDisplay.js
@@ -3,9 +3,9 @@ app.component('code-display', {
         `<div id="code-field">
         <p
             v-for="(line, line_idx) in text"
-            :style="{ marginLeft: getIndent(line_idx) + 'px' }">
+            :style="{ marginLeft: getIndent(line_idx) + 'px' , opacity: getOpacity(line_idx)}">
             <span
-                v-for="(character, char_idx) in line.content"     
+                v-for="(character, char_idx) in line.content"    
                 :class="getClassAt(line_idx, char_idx)"> 
                 {{ character }}
             </span>
@@ -13,11 +13,18 @@ app.component('code-display', {
         </div>`,
     data() {
         return {
-            text: [
+            text1: [
                 {line_idx: 0, content: "public static void main (String[] args) {", indent: 0},
                 {line_idx: 1, content: "System.out.println(\"Hello World!\");", indent: 1},
-                {line_idx: 3, content: "}", indent: 0},
+                {line_idx: 3, content: "}", indent: 0}
             ],
+            text2: [
+                {line_idx: 0, content: "pubic static hair (String[] tanga) {", indent: 0},
+                {line_idx: 1, content: "System.out.println(\"Sadi Gali!\");", indent: 1},
+                {line_idx: 3, content: "}", indent: 0}
+            ],
+            text: "",
+            INDENT_PX: 25,
             currentLine: 0,
             cursorPosition: 0,
             charsTyped: [],
@@ -26,7 +33,14 @@ app.component('code-display', {
     },
     methods: {
         getIndent(line_index) {
-            return this.text[line_index].indent * INDENT_PX
+            return this.text[line_index].indent * this.INDENT_PX
+        },
+        getOpacity(line_index) {
+            if (line_index <= this.currentLine) {
+                return "100%"
+            } else {
+                return "10%"
+            }
         },
         onkeydown(event) {
             let key = event.key
@@ -35,7 +49,7 @@ app.component('code-display', {
                     this.reverseCursor()
                 }
             } else if (key === "Enter") {
-                if (this.cursorPosition >= this.currentLineLength) {
+                if (this.cursorPosition >= this.currentLineLength - 1) {
                     this.newLine()
                 } else {
                     this.charsTyped[this.currentLine].push(key)
@@ -72,7 +86,7 @@ app.component('code-display', {
                 this.keysTyped.push("Backspace")
                 this.cursorPosition--
             }
-        }
+        },
     },
     computed: {
         currentLineLength() {
@@ -80,11 +94,17 @@ app.component('code-display', {
         },
         currentLineText() {
             return this.text[this.currentLine].content
-        }
+        },
     },
-    created() {
+    created(){
+        // get snippet from server
+        this.text = this.text1;
+        for (let l = 0; l < this.text.length; l++) {
+            this.text[l].content = this.text[l].content += "↵"
+        }
+
         document.onkeydown = this.onkeydown
-        // getText()
+
         for (let l = 0; l < this.text.length; l++) {
             this.charsTyped[l] = []
         }
@@ -121,82 +141,6 @@ const text4 = [
 
 ]
 
-const texts = [text1, text2, text3, text4]
-let text_old = text1
-
-const INDENT_PX = 25
-const HIGHLIGHT_COLOR = "#e79f0b"
-const CORRECT_COLOR = "#00ff00"
-const INCORRECT_COLOR = "#ff0000"
-
-let paras = {}
-let line_idx = 0
-let current_line = text_old[line_idx][0]
-let cursor_pos = 0
-let history = {}
-let return_next = false
-
-window.onload = function (event) {
-    get_new_snippet()
-};
-
-document.addEventListener('keydown', function (event) {
-    return
-    let key = event.key
-
-    if (key === current_line.charAt(cursor_pos)) {
-        forwardCursor(true)
-    } else {
-        if (key === "Backspace") {
-            if (line_idx != 0 || cursor_pos != 0) {
-                reverseCursor()
-            }
-        } else if (key === "Enter") {
-            if (cursor_pos >= current_line.length) {
-                newLine()
-            } else {
-                forwardCursor(false)
-            }
-        } else if (key.length === 1) {
-            forwardCursor(false)
-        }
-    }
-
-});
-
-
-function displaySnippet() {
-    document.getElementById("code_field").innerHTML = ''
-    for (l = 0; l < text_old.length; l++) {
-        line = text_old[l]
-        let para = document.createElement("p")
-        paras[l] = para
-        if (line[1] > 0) {
-            indent = line[1] * INDENT_PX
-            para.style.marginLeft = indent + "px"
-        }
-        let text_node = document.createTextNode(line[0])
-        para.appendChild(text_node);
-        document.getElementById("code_field").appendChild(para)
-    }
-    updatePara()
-}
-
-function forwardCursor(correct) {
-    if (cursor_pos + 1 >= current_line.length) {
-        return_next = true
-        updatePara()
-        return
-    }
-    cursor_pos++
-    if (correct) {
-        current_history.push(true)
-    } else {
-        current_history.push(false)
-    }
-    updatePara()
-}
-
 // function reverseCursor() {
 //     if (cursor_pos === 0 && line_idx != 0) {
 //         paras[line_idx].innerHTML = current_line
@@ -228,57 +172,25 @@ function forwardCursor(correct) {
 //     updatePara()
 // }
 
-function updatePara() {
-    innerHTML = ""
-    for (i = 0; i < current_history.length; i++) {
-        innerHTML +=
-            '<span style=\"color:' + getColor(current_history[i]) + ';\">'
-            + current_line.slice(i, i + 1)
-            + '</span>'
-    }
-
-    if (cursor_pos < current_line.length) {
-        innerHTML += '<span style=\"background-color:' + HIGHLIGHT_COLOR + ';\">'
-            + current_line.slice(cursor_pos, cursor_pos + 1)
-            + '</span>'
-            + current_line.slice(cursor_pos + 1, current_line.length)
-    }
-
-
-    if (return_next) {
-        innerHTML += '<span style=\"background-color:' + HIGHLIGHT_COLOR + ';\">' + "↵" + '</span>'
-    }
-
-    paras[line_idx].innerHTML = innerHTML
-}
-
-function getColor(correct) {
-    if (correct) {
-        return CORRECT_COLOR
-    } else {
-        return INCORRECT_COLOR
-    }
-}
-
 
 //for testing
-const get_new_snippet = async () => {
-    text_old = randomChoice(texts)
-    paras = {}
-    line_idx = 0
-    current_line = text_old[line_idx][0]
-    cursor_pos = 0
-    history = []
-    for (i = 0; i < text_old.length; i++) {
-        history[i] = []
-    }
-    current_history = history[line_idx]
-    displaySnippet()
-}
-
-function randomChoice(arr) {
-    return arr[Math.floor(Math.random() * arr.length)];
-}
+// const get_new_snippet = async () => {
+//     text_old = randomChoice(texts)
+//     paras = {}
+//     line_idx = 0
+//     current_line = text_old[line_idx][0]
+//     cursor_pos = 0
+//     history = []
+//     for (i = 0; i < text_old.length; i++) {
+//         history[i] = []
+//     }
+//     current_history = history[line_idx]
+//     displaySnippet()
+// }
+//
+// function randomChoice(arr) {
+//     return arr[Math.floor(Math.random() * arr.length)];
+// }
 
 // dude prolly sth with this
 // POST: daraus den aufruf bauen, den wir brauchen sobald ein Code abgeschlossen ist
