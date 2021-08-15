af2cbede98f540142b57d520231ec4c485fc9ce1
Julius W <julius.wunderlich@web.de>
2021-08-15 23:05:03 +0200

    add script to populate :fire: store

--- backend/utils/database_populator.py
+++ backend/utils/database_populator.py
@@ -1,11 +1,14 @@
+import json
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-# Use the application default credentials
-cred = credentials.ApplicationDefault()
-firebase_admin.initialize_app(cred, {
-    'projectId': "speeddev-b728a"
-})
+cred = credentials.Certificate("./speeddev_creds.json")
+firebase_admin.initialize_app(cred)
 
 db = firestore.client()
+
+with open ('./Codes/codes_java.json', 'r') as file:
+    snippets = json.load(file)
+    for snippet in snippets:
+        db.collection(u'snippets').document(snippet['title']).set(snippet)
